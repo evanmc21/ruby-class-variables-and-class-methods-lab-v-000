ba17949d4261919c0cfa0c311bcec57dcacc33ed
Evan Candler <emcandler21@gmail.com>
2018-02-26 20:22:17 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :name, :artist, :genre
+    @@genres = []
+    @@artists = []
+    @@count = 0
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  def self.count
+    @@count
+  end
+
+end
